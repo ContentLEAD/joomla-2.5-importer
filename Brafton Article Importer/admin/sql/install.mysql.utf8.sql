1793dfacc94bf97fd7bc0f9153663e2dd493abc0
Michael Walton <michael.walton@contentlead.com>
2012-08-31 17:00:46 -0400

    v0.2

--- Brafton Article Importer/admin/sql/install.mysql.utf8.sql
+++ Brafton Article Importer/admin/sql/install.mysql.utf8.sql
@@ -3,23 +3,23 @@ DROP TABLE IF EXISTS `#__brafton_content`;
 DROP TABLE IF EXISTS `#__brafton_options`;
  
 CREATE TABLE `#__brafton_categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `cat_id` int(11) NOT NULL,
-  `brafton_cat_id` int(11) NOT NULL,
+  `brafton_cat_id` int(10) NOT NULL,
    PRIMARY KEY  (`id`),
-   FOREIGN KEY (`cat_id`) REFERENCES #__categories(`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+   FOREIGN KEY (`cat_id`) REFERENCES #__categories(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0;
 
 CREATE TABLE `#__brafton_content` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content_id` int(11) NOT NULL,
-  `brafton_content_id` int(11) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `content_id` int(10) NOT NULL,
+  `brafton_content_id` int(10) NOT NULL,
    PRIMARY KEY  (`id`),
-   FOREIGN KEY (`content_id`) REFERENCES #__content(`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+   FOREIGN KEY (`content_id`) REFERENCES #__content(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0;
 
 CREATE TABLE `#__brafton_options` (
   `option` varchar(100) NOT NULL,
   `value` varchar(500) NOT NULL,
    PRIMARY KEY  (`option`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0;
