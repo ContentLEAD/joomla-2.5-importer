4cd7351f47903835a33d53ea23b9ebbf33996bb5
brian <brian.corey@contentlead.com>
2013-01-21 13:46:29 -0500

    Fixed SQL error 1005 install bug
    Patched install script to use default engine and AUTO_INCREMENT settings

--- Brafton Article Importer/admin/sql/install.mysql.utf8.sql
+++ Brafton Article Importer/admin/sql/install.mysql.utf8.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `#__brafton_categories` (
   `brafton_cat_id` int(10) NOT NULL,
    PRIMARY KEY  (`id`),
    FOREIGN KEY (`cat_id`) REFERENCES #__categories(`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=0;
+);
 
 CREATE TABLE IF NOT EXISTS `#__brafton_content` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
@@ -12,12 +12,12 @@ CREATE TABLE IF NOT EXISTS `#__brafton_content` (
   `brafton_content_id` int(10) NOT NULL,
    PRIMARY KEY  (`id`),
    FOREIGN KEY (`content_id`) REFERENCES #__content(`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=0;
+);
 
 CREATE TABLE IF NOT EXISTS `#__brafton_options` (
   `option` varchar(100) NOT NULL,
   `value` varchar(500) NOT NULL,
    PRIMARY KEY  (`option`)
-) ENGINE=InnoDB AUTO_INCREMENT=0;
+);
 
 INSERT IGNORE INTO  `#__brafton_options` (`option`,`value`) VALUES ('api-key',  ''), ('base-url',  ''), ('author', ''), ('import-order', '');
