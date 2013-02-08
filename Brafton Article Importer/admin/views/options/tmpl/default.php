7c2726fa92768c1791902def405d2f11245f90ff
Brian Corey <brian.corey@contentlead.com>
2013-02-08 14:45:44 -0500

    v0.6.3 - Restyled settings page; separated API Domain and API Key.

--- Brafton Article Importer/admin/views/options/tmpl/default.php
+++ Brafton Article Importer/admin/views/options/tmpl/default.php
@@ -4,69 +4,117 @@ defined('_JEXEC') or die('Restricted access');
 JHTML::_('behavior.tooltip');
 ?>
 <form action="index.php?option=com_braftonarticles" method="post" name="adminForm">
-<table align="left">
-	<tr>
-	<td>Hover over a title for futher information on that field.</td>
-	</tr>
-	<tr>
-		<td><?php echo JHTML::tooltip('This is the url/api key provided by Brafton/ContentLEAD which is used to import articles.  It should look something like<br/><b>http://api.brafton.com/eee83d24-906b-4736-91d9-1031621b85eb/</b>', 'API URL', '', '<h2 class=admin-header>API URL</h2>'); ?>	
-			Please enter the full URL for your content below<br/>
-			<input type="text" name="api-key" size="75" value="<?php echo $this->base_url . $this->api_key; ?>"/> <br />
-		</td>
-  	</tr>
-	<tr>
-		<td>
-		<?php echo JHTML::tooltip('Changing the author after some articles have been uploaded 
-		will ONLY change the author for recently uploaded articles, not the older ones.  This is (currently) working as intended.', 'Consider the Following', '', '<h2 class=admin-header>Post Author</h2>'); ?>
-		Sets the post author for all imported entires<br/><br/>
-			<select name="author">
-				<?php foreach($this->authorList as $author): ?>
+<div id="brafton-standard-opts">
+	<h2>Settings</h2>
+	<input type="hidden" name="task" value="options.apply" />
+	<div class="setting">
+		<h3>
+		<?php
+			echo JHTML::tooltip(
+				'Your unique API key, in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.',
+				'API Key',
+				'',
+				'API Key');
+		?>
+		</h3>
+		<input type="text" name="api-key" value="<?php echo $this->api_key; ?>"/>
+	</div>
+
+	<div class="setting">
+		<h3>
+		<?php
+			echo JHTML::tooltip(
+				'The base URL of your news feed.',
+				'API Domain',
+				'',
+				'API Domain');
+		?>
+		</h3>
+		<select name="base-url">
+			<?php
+				$opts = array('http://api.brafton.com/', 'http://api.contentlead.com/', 'http://api.castleford.com.au/');
+				foreach ($opts as $o) : ?>
 				<option 
-					<?php if(($this->author) == $author->id): ?>
-						 selected="selected"
+					<?php if ($this->base_url == $o) : ?>
+						selected="selected"
 					<?php endif; ?>
-						value="<?php echo $author->id; ?>"><?php echo $author->name; ?>
+				value="<?php echo $o; ?>"><?php echo $o; ?>
+			<?php endforeach; ?>
+		</select>
+	</div>
+
+	<div class="setting">
+		<h3>
+		<?php
+			echo JHTML::tooltip(
+				'The author that articles will be attributed to.',
+				'Post Author',
+				'',
+				'Post Author');
+		?>
+		</h3>
+		<select name="author">
+			<?php foreach($this->authorList as $author): ?>
+			<option 
+				<?php if(($this->author) == $author->id): ?>
+					 selected="selected"
+				<?php endif; ?>
+					value="<?php echo $author->id; ?>"><?php echo $author->name; ?>
+			</option>
+			<?php endforeach; ?>
+		</select>
+	</div>
+</div>
+
+<fieldset>
+	<legend><h2>Advanced</h2> (<a href="#" onclick="$$('div#brafton-advanced-opts').toggle();">Show/Hide</a>)</legend>
+	<div id="brafton-advanced-opts" style="display: none;">
+		<div class="setting">
+			<h3>
+			<?php
+				echo JHTML::tooltip(
+					'The article\'s Create Date is set based on this setting.',
+					'Article Date',
+					'',
+					'Article Date');
+			?>
+			</h3>
+			<select name="import-order">
+				<?php
+					$opts = array('Published Date', 'Last Modified Date', 'Created Date');
+					foreach ($opts as $o) : ?>
+					<option 
+						<?php if ($this->importOrder == $o) : ?>
+							selected="selected"
+						<?php endif; ?>
+					value="<?php echo $o; ?>"><?php echo $o; ?>
 				</option>
 				<?php endforeach; ?>
 			</select>
-		</td>
-		<input type="hidden" name="task" value="options.apply" />
-	</tr>
-	
-	<tr>
-		<td>
-			<fieldset>
-				<legend>Advanced Options (<a href="#" onclick="$$('div#brafton-advanced-opts').toggle();">Show/Hide</a>)</legend>
-				<div id="brafton-advanced-opts" style="display: none;">
-					<?php echo JHTML::tooltip('Sets the article\'s created date based on the following', 'Import Order', '', '<h2 class=admin-header>Import Order</h2>'); ?>
-					<select name="import-order">
-						<?php
-							$opts = array('Published Date', 'Last Modified Date', 'Created Date');
-							foreach ($opts as $o) : ?>
-							<option 
-								<?php if ($this->importOrder == $o) : ?>
-									selected="selected"
-								<?php endif; ?>
-							value="<?php echo $o; ?>"><?php echo $o; ?>
-						</option>
-						<?php endforeach; ?>
-					</select>
-					
-					<?php echo JHTML::tooltip('The article\'s published status when imported', 'Published Status', '', '<h2 class=admin-header>Published Status</h2>'); ?>
-					<select name="published-state">
-						<?php
-							$opts = array('Published', 'Unpublished');
-							foreach ($opts as $o) : ?>
-							<option 
-								<?php if ($this->publishedState == $o) : ?>
-									selected="selected"
-								<?php endif; ?>
-							value="<?php echo $o; ?>"><?php echo $o; ?>
-						<?php endforeach; ?>
-					</select>
-				</div>
-			</fieldset>
-		</td>
-	</tr>
-</table>
+		</div>
+		
+		<div class="setting">
+			<h3>
+			<?php
+				echo JHTML::tooltip(
+					'The article\'s published status when imported.',
+					'Published Status',
+					'',
+					'Published Status');
+			?>
+			</h3>
+			<select name="published-state">
+				<?php
+					$opts = array('Published', 'Unpublished');
+					foreach ($opts as $o) : ?>
+					<option 
+						<?php if ($this->publishedState == $o) : ?>
+							selected="selected"
+						<?php endif; ?>
+					value="<?php echo $o; ?>"><?php echo $o; ?>
+				<?php endforeach; ?>
+			</select>
+		</div>
+	</div>
+</fieldset>
 </form>
