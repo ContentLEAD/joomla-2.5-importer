381aff3bf008b54ac9b32b130129c4bfdb392f49
Brian Corey <brian.corey@contentlead.com>
2013-02-14 17:02:31 -0500

    Added Joomla version check.

--- Brafton Article Importer/com_braftonarticles.script.php
+++ Brafton Article Importer/com_braftonarticles.script.php
@@ -3,38 +3,57 @@ defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.installer.installer');
 jimport('joomla.filesystem.file');
+jimport('joomla.log.log');
 
 class com_braftonarticlesInstallerScript
-{ 
-        /**
-         * Called after any type of action
-         *
-         * @param   string  $route  Which action is happening (install|uninstall|discover_install)
-         * @param   JAdapterInstance  $adapter  The object responsible for running this script
-         *
-         * @return  boolean  True on success
-         */
-        public function postflight($type, $parent){ 
+{
+	public function __construct()
+	{
+		JLog::addLogger(array('text_file' => 'com_braftonarticles.log.php'), JLog::ALL, 'com_braftonarticles');
+	}
+	
+	public function preflight($type, $parent)
+	{
+		$v = new JVersion();
+		$joomlaVersion = $v->getShortVersion();
+		
+		if (version_compare($joomlaVersion, '2.5', 'lt') || version_compare($joomlaVersion, '3.0', 'gte'))
+		{
+			$app = JFactory::getApplication();
+			$app->enqueueMessage(sprintf('%s failed: This component requires Joomla 2.5.', ucfirst($type)), 'error');
+			return false;
+		}
+	}
+	
+	/**
+	 * Called after any type of action
+	 *
+	 * @param   string  $route  Which action is happening (install|uninstall|discover_install)
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function postflight($type, $parent){ 
+		$installer = new JInstaller;
+		$src = $parent->getParent()->getPath('source');
+		$installer->install($src.DS.'plg_braftoncron');
+	}
+
+	/**
+	 * Called on uninstallation
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 */
+	public function uninstall(JAdapterInstance $adapter) {
+		// Uninstalls s system plugin named plg_myplugin
+		$db = JFactory::getDBO();
+		$db->setQuery('SELECT `extension_id` FROM #__extensions WHERE `element` = "braftoncron" AND `folder` = "system"');
+		$id = $db->loadResult();
+		if($id)
+		{
 			$installer = new JInstaller;
-			$src = $parent->getParent()->getPath('source');
-			$installer->install($src.DS.'plg_braftoncron');
+			$result = $installer->uninstall('plugin',$id,1);
+			$status->plugins[] = array('name'=>'plg_srp','group'=>'system', 'result'=>$result);
 		}
- 
-        /**
-         * Called on uninstallation
-         *
-         * @param   JAdapterInstance  $adapter  The object responsible for running this script
-         */
-        public function uninstall(JAdapterInstance $adapter) {
-			// Uninstalls s system plugin named plg_myplugin
-			$db = JFactory::getDBO();
-			$db->setQuery('SELECT `extension_id` FROM #__extensions WHERE `element` = "braftoncron" AND `folder` = "system"');
-			$id = $db->loadResult();
-			if($id)
-			{
-				$installer = new JInstaller;
-				$result = $installer->uninstall('plugin',$id,1);
-				$status->plugins[] = array('name'=>'plg_srp','group'=>'system', 'result'=>$result);
-			}
-		}
+	}
 }
