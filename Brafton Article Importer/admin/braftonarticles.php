876966a5926818f5d723c988d7b0101dedd9e192
brian <brian.corey@contentlead.com>
2013-03-20 14:21:41 -0400

    Added fallbacks for Platform 11.4 dependencies
    JInput is not included with Joomla 1.7; added calls to JRequest.

--- Brafton Article Importer/admin/braftonarticles.php
+++ Brafton Article Importer/admin/braftonarticles.php
@@ -1,24 +1,32 @@
 <?php
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
-// import joomla controller library
+
 jimport('joomla.application.component.controller');
+jimport('joomla.version');
 
-
-/*
-Load controllers here...
-*/
-// Get an instance of the controller prefixed by HelloWorld
 $controller = JController::getInstance('BraftonArticles');
 
-$jinput = JFactory::getApplication()->input;
-$view = strtolower($jinput->get('view', 'options'));
+$version = new JVersion();
+$joomlaVersion = $version->getShortVersion();
+$view = '';
+$task = '';
+
+if (version_compare($joomlaVersion, '2.5', '<'))
+{
+	$view = strtolower(JRequest::getVar('view', 'options'));
+	$task = JRequest::getCmd('task');
+}
+else
+{
+	$jinput = JFactory::getApplication()->input;
+	$view = strtolower($jinput->get('view', 'options'));
+	$task = $jinput->get('task');
+}
 JSubMenuHelper::addEntry('Settings', 'index.php?option=com_braftonarticles', $view == 'options');
 JSubMenuHelper::addEntry('Log', 'index.php?option=com_braftonarticles&view=log', $view == 'log');
- 
-// Perform the Request task
-$controller->execute(JRequest::getCmd('task'));
+
+$controller->execute($task);
  
 // Redirect if set by the controller
 $controller->redirect();
